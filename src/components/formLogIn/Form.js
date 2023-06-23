c845c297ea84e2bda29fbbf341961a34f775a2e0
José Sanmiguel <jose.sanmiguel.12@gmail.com>
2023-06-22 21:49:06 -0500

    dañando login

--- src/components/formLogIn/Form.js
+++ src/components/formLogIn/Form.js
@@ -1,5 +1,4 @@
-import React, {useEffect, useState} from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react'
 // import { login, logout} from '../redux/actions/authActions';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
@@ -7,8 +6,6 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { UnlockIcon } from '@chakra-ui/icons'
 // import Swal from "sweetalert2";
 import {
-    FormControl,
-    FormLabel,
     FormErrorMessage,
     Input,
     Flex,
@@ -55,28 +52,28 @@ const Form = () => {
     return (
         <ChakraProvider>
             <form  position="absolute">
-                <Flex  overflow="wrap" flexWrap="wrap" alignContent="start" flexDirection="column" spacing={10}>
-                <Stack spacing={4} alignItems="center">
-                    <InputGroup display="flex" alignItems="center" >
-                        <Input  display="flex" alignItems="center" variant='flushed'  type="text" name="username" color="black"  placeholder='Usuario' _placeholder={{ opacity: 1, color: 'gray', fontSize:"small" }}{...formik.getFieldProps('username')}  onChange={formik.handleChange} value={formik.values.username}/>
-                        <FormErrorMessage>{formik.touched.username && formik.errors.username &&(<div>{formik.errors.username}</div>) }</FormErrorMessage>
-                        <InputLeftElement top= "-28px" height="100px" >
-                            <AiOutlineUser color="black" />
-                        </InputLeftElement>
-                        
-                    </InputGroup>
-                    <InputGroup display="flex" alignItems="center">
-                        <InputLeftElement pointerEvents='none' top= "0px">
-                            <UnlockIcon  color="black" />
-                        </InputLeftElement>
-                        <Input display="flex" alignItems="center" variant='flushed'  type="password" name="password" color="black"placeholder="Contraseña" _placeholder={{ opacity: 1, color: 'gray', fontSize:"small" }}{...formik.getFieldProps('password')}/>
-                        <FormErrorMessage>{formik.touched.password && formik.errors.password && <div>{formik.errors.password}</div>}</FormErrorMessage>
-                    </InputGroup>
-                </Stack>
-                <Button  ml={5} width='90%' size='lg' mt={7} bg='#FFE031' variant='solid' type="submit" disabled={formik.isSubmitting}>
-                    Iniciar sesión
-                </Button>
-                </Flex >
+            <Flex  overflow="wrap" flexWrap="wrap" alignContent="start" flexDirection="column" spacing={10}>
+            <Stack spacing={4} alignItems="center">
+                <InputGroup display="flex" alignItems="center" >
+                    <Input  display="flex" alignItems="center" variant='flushed'  type="text" name="username" color="black"  placeholder='Usuario' _placeholder={{ opacity: 1, color: 'gray', fontSize:"small" }}{...formik.getFieldProps('username')}  onChange={formik.handleChange} value={formik.values.username}/>
+                    <FormErrorMessage>{formik.touched.username && formik.errors.username &&(<div>{formik.errors.username}</div>) }</FormErrorMessage>
+                    <InputLeftElement top= "-28px" height="100px" >
+                        <AiOutlineUser color="black" />
+                    </InputLeftElement>
+                    
+                </InputGroup>
+                <InputGroup display="flex" alignItems="center">
+                    <InputLeftElement pointerEvents='none' top= "0px">
+                        <UnlockIcon  color="black" />
+                    </InputLeftElement>
+                    <Input display="flex" alignItems="center" variant='flushed'  type="password" name="password" color="black"placeholder="Contraseña" _placeholder={{ opacity: 1, color: 'gray', fontSize:"small" }}{...formik.getFieldProps('password')}/>
+                    <FormErrorMessage>{formik.touched.password && formik.errors.password && <div>{formik.errors.password}</div>}</FormErrorMessage>
+                </InputGroup>
+            </Stack>
+            <Button  ml={5} width='90%' size='lg' mt={7} bg='#FFE031' variant='solid' type="submit" disabled={formik.isSubmitting}>
+                Iniciar sesión
+            </Button>
+            </Flex >
             </form>
         </ChakraProvider >
     )
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,35 @@
 import React from "react";
 import logo from "../assets/logo.svg";
-import Form from "../components/formLogIn/Form";
-import { Button, HStack, Stack, Text } from "@chakra-ui/react";
+import { Button, Flex, FormErrorMessage, HStack, Input, InputGroup, InputLeftElement, Stack, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { useDispatch } from "react-redux";
 import { loginGoogle } from "../redux/actions/authActions";
+import { AiOutlineUser } from "react-icons/ai"
+import { UnlockIcon } from '@chakra-ui/icons'
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+      .min(2, 'El nombre de usuario debe tener al menos 3 caracteres')
+      .max(15, 'El nombre de usuario no debe tener más de 15 caracteres')
+      .required('El nombre de usuario es obligatorio'),
+  password: Yup.string()
+      .min(2, 'La contraseña debe tener al menos 2 caracteres')
+      .required('La contraseña es obligatoria')
+});
+const initialValues = {
+  username: '',
+  password: '',
+};
 
 const Login = () => {
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+  });
+
   const dispatch = useDispatch();
 
   const handleLoginGoogle = () => {
@@ -23,10 +45,31 @@ const Login = () => {
         <Text textAlign="center">
           Login or create an account with your phone number to start ordering
         </Text>
-        {/* <Form handleUser={handleUser} /> */}
 
-        <Form />
+
+        <form  position="absolute">
+            <Flex  overflow="wrap" flexWrap="wrap" alignContent="start" flexDirection="column" spacing={10}>
+            <Stack spacing={4} alignItems="center">
+                <InputGroup display="flex" alignItems="center" >
+                    <Input  display="flex" alignItems="center" variant='flushed'  type="text" name="username" color="black"  placeholder='Usuario' _placeholder={{ opacity: 1, color: 'gray', fontSize:"small" }}{...formik.getFieldProps('username')}  onChange={formik.handleChange} value={formik.values.username}/>
+                    <FormErrorMessage>{formik.touched.username && formik.errors.username &&(<div>{formik.errors.username}</div>) }</FormErrorMessage>
+                    <InputLeftElement top= "-28px" height="100px" >
+                        <AiOutlineUser color="black" />
+                    </InputLeftElement>
+                    
+                </InputGroup>
+                <InputGroup display="flex" alignItems="center">
+                    <InputLeftElement pointerEvents='none' top= "0px">
+                        <UnlockIcon  color="black" />
+                    </InputLeftElement>
+                    <Input display="flex" alignItems="center" variant='flushed'  type="password" name="password" color="black"placeholder="Contraseña" _placeholder={{ opacity: 1, color: 'gray', fontSize:"small" }}{...formik.getFieldProps('password')}/>
+                    <FormErrorMessage>{formik.touched.password && formik.errors.password && <div>{formik.errors.password}</div>}</FormErrorMessage>
+                </InputGroup>
+            </Stack>
+            </Flex >
+            </form>
         
+
         <h2>Restablecer contraseña</h2>
         <HStack my={5}>
           <Text>Inicia sesión con</Text>
